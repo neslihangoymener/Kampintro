55680a4a7908d07132fec98f54172102db9959b9
Neslihan Kuş Göymener <neslihan_kus@hotmail.com>
2021-01-23 09:33:47 +0300

    Generics - MyDictionary

---
+++ Generics/MyDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class MyDictionary<TKey, TValue>
+    {
+        List<TKey> _keys;
+        List<TValue> _values;
+
+        public MyDictionary()
+        {
+            _keys = new List<TKey>();
+            _values = new List<TValue>();
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            _keys.Add(key);
+            _values.Add(value);
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                int index = _keys.IndexOf(key);
+                TValue value = _values[index];
+
+                return value;
+            }
+        }
+    }
+    
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> AdYas = new Dictionary<int, string>();
+            AdYas.Add(41, "Neslihan");
+            AdYas.Add(43, "Murat");
+
+            Console.WriteLine(AdYas[41]);
+
+            foreach (var item in AdYas)
+            {
+                Console.WriteLine(item);
+            }
+
+            MyDictionary<int,string> AdYas2 = new MyDictionary<int, string>();
+
+            AdYas2.Add(15, "Azra");
+            AdYas2.Add(11, "Emir");
+
+            
+            Console.WriteLine(AdYas2[15] + " 15 yaşındadır");
+            Console.WriteLine(AdYas2[11] +" 11 yaşındadır");
+
+            Console.ReadLine();
+        }
+    }
+    
+}
